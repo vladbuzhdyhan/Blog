3a9e99452014b2b5230ffacbaecf4673dc680d86
vladbuzhdyhan <vladyslavbuzhdyhan@gmail.com>
2023-12-07 21:39:48 +0200

    Search

---
+++ WebApplication3/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly SiteContext _siteContext;
+        public SearchController(SiteContext siteContext)
+        {
+            _siteContext = siteContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Find(string title)
+        {
+            var found = _siteContext.News.Where(x => x.Title == title).OrderByDescending(x => x.Date).ToList();
+            return View(found);
+        }
+    }
+}
